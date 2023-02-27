2b1b6073cb7508258cfef0b04497e42fc0fe4cd0
m-slezak <77272905+m-slezak@users.noreply.github.com>
2023-02-27 18:54:22 +0100

    add new array of dices, add prettier

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,26 +1,31 @@
-import Die from "./components/Die"
+import Die from "./components/Die";
 
 function App() {
-
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+  console.log(allNewDice());
 
   return (
-   <main>
-   <section className="dieContainer">
-    <Die />
-    <Die />
-    <Die />
-    <Die />
-    <Die />
-    <Die />
-    <Die />
-    <Die />
-    <Die />
-    <Die />
-   </section>
-
-
-   </main>
-  )
+    <main>
+      <section className="dieContainer">
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
